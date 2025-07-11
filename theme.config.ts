ac0782c6ac3cc6dbf3f065135edf2f46aff44db6
Sean Kennedy <sean@codeserious.com>
2025-07-11 17:53:52 -0400

    Initial wordbun setup

---
+++ theme.config.ts
@@ -0,0 +1,15 @@
+/**
+ * Wordbun theme configuration
+ */
+
+import type { ThemeConfig } from '~types/config';
+
+const themeConfig: Partial<ThemeConfig> = {
+  colors: {
+    primary: '#6b2d6b',
+    primaryLight: '#a678b8',
+    primaryDark: '#4b184b',
+  },
+};
+
+export default themeConfig;
